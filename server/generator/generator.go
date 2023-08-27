7c6fc87bca6c32002bd7834bb0e0f9436cb0171b
Vít Listík <tivvit@seznam.cz>
2023-08-27 17:23:18 +0200

    implement new generator features

--- server/generator/generator.go
+++ server/generator/generator.go
@@ -1,7 +1,6 @@
 package generator
 
 import (
-	"os"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -10,22 +9,19 @@ import (
 )
 
 const binary = "./generator/dntgenerator"
-const outfile = "/tmp/level.json"
 
 func Generate_level(start int, end int, max int) string {
-	cmd := exec.Command(binary, "-s", strconv.Itoa(start), "-e", strconv.Itoa(end), "-m", strconv.Itoa(max), "-j", outfile)
+	cmd := exec.Command(binary, "-s", strconv.Itoa(start), "-e", strconv.Itoa(end), "-m", strconv.Itoa(max), "-j", "-", "-h", "")
 
 	stderr := &strings.Builder{}
+	stdout := &strings.Builder{}
 	cmd.Stderr = stderr
+	cmd.Stdout = stdout
 
 	if err := cmd.Run(); err != nil {
 		log.Warn().Msgf("stderr: %s", stderr.String())
 		log.Fatal().Msgf("failed to run %s: %v", binary, err)
 	}
 
-	dat, err := os.ReadFile(outfile)
-	if err != nil {
-		log.Fatal().Msgf("failed to read generated file %s: %v", outfile, err)
-	}
-	return string(dat)
+	return stdout.String()
 }
