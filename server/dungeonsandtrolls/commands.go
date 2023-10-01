5629a61296c82b27b61608010144ce8862da3ea6
Vít Listík <tivvit@seznam.cz>
2023-10-01 20:35:38 +0200

    none attack type

--- server/dungeonsandtrolls/commands.go
+++ server/dungeonsandtrolls/commands.go
@@ -312,30 +312,29 @@ func EvaluateEffects(g *Game, effects []*api.Effect, a *api.Attributes, receiver
 		if err != nil {
 			return keptEffects, err
 		}
-		// TODO enable when ready
-		//if e.DamageType != api.DamageType_none {
-		damage := gameobject.EvaluateDamage(float64(e.DamageAmount), e.DamageType, a)
+		if e.DamageType != api.DamageType_none {
+			damage := gameobject.EvaluateDamage(float64(e.DamageAmount), e.DamageType, a)
 
-		var attackerName string
-		if e.XCasterId != nil {
-			attacker, err := g.GetObjectById(*e.XCasterId)
-			if err == nil {
-				attackerName = attacker.GetName()
+			var attackerName string
+			if e.XCasterId != nil {
+				attacker, err := g.GetObjectById(*e.XCasterId)
+				if err == nil {
+					attackerName = attacker.GetName()
+				}
 			}
-		}
 
-		if damage > 0 {
-			receiver.DamageTaken()
-		}
+			if damage > 0 {
+				receiver.DamageTaken()
+			}
 
-		damageEvent := api.Event_DAMAGE
-		g.LogEvent(&api.Event{
-			Type:        &damageEvent,
-			Message:     fmt.Sprintf("%s (%s): damaged %s (%s): %f with %s", *e.XCasterId, attackerName, receiver.GetId(), receiver.GetName(), damage, e.DamageType.String()),
-			Damage:      &damage,
-			Coordinates: receiver.GetPosition(),
-		})
-		//}
+			damageEvent := api.Event_DAMAGE
+			g.LogEvent(&api.Event{
+				Type:        &damageEvent,
+				Message:     fmt.Sprintf("%s (%s): damaged %s (%s): %f with %s", *e.XCasterId, attackerName, receiver.GetId(), receiver.GetName(), damage, e.DamageType.String()),
+				Damage:      &damage,
+				Coordinates: receiver.GetPosition(),
+			})
+		}
 
 		// TODO do not go over max
 
