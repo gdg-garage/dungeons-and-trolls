452b2c3dcd00d2c564be12e6b32475257ef8fcba
Vít Listík <tivvit@seznam.cz>
2023-10-10 04:16:01 +0200

    los debug

--- server/dungeonsandtrolls/handlers/skill.go
+++ server/dungeonsandtrolls/handlers/skill.go
@@ -86,9 +86,9 @@ func validateSkill(game *dungeonsandtrolls.Game, skillUse *api.SkillUse, p gameo
 				log.Info().Msgf("map cell los %+v", resultMap[gameobject.PlainPosFromApiPos(objects.Position)])
 			}
 
-			if !gameobject.GetLoS(currentLevel, resultMap, map[float32]float32{}, gameobject.CoordinatesToPosition(p.GetPosition()), targetPos) {
-				return fmt.Errorf("target is not in los")
-			}
+			//if !gameobject.GetLoS(currentLevel, resultMap, map[float32]float32{}, gameobject.CoordinatesToPosition(p.GetPosition()), targetPos) {
+			//	return fmt.Errorf("target is not in los")
+			//}
 		}
 		if s.Flags.RequiresOutOfCombat {
 			if p.GetLastDamageTaken() < 3 {
